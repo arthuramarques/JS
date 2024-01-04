3500caacbe7c5634bd55fc2680b4acd9b348d33f
arthuramarques <jonasnpereira09@gmail.com>
2024-01-04 16:46:53 -0300

    relacionais_2 complemento

--- Fundamentos/Operadores/relacionais_2.js
+++ Fundamentos/Operadores/relacionais_2.js
@@ -5,7 +5,9 @@ let c = "1";
 
 console.log(a ==b);
 
-// comparação estrita: interessa saber se os conteúdos são do mesmo tipo e se os conteúdos são iguais
-console.log(a === b); 
+// comparação estrita: interessa saber se os conteúdos e os tipos são iguais ou diferentes
+console.log(a === b); // igualdade estrita
 console.log(a != b);
-console.log(a !== b);
+console.log(a !== b); // diferença estrita 
+
+// usa-se a comparação estrita com maior recorrência em virtude de uma maior segurança ao código
