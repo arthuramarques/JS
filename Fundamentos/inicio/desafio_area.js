65844735db500d2a0517d9b36fe28cd47b4cb862
arthuramarques <jonasnpereira09@gmail.com>
2024-01-04 15:01:44 -0300

    desafio_areaCirc

---
+++ Fundamentos/inicio/desafio_area.js
@@ -0,0 +1,9 @@
+// PI --> usa-se letras maiusculas para constantes
+// PI * raio * raio
+
+let raio = 20;
+const PI = 3.141592;
+let areaCircunferencia = raio * raio * PI; 
+
+console.log("O valor da áre é igual a " + areaCircunferencia + " m²");
+console.log(Math.PI);
