07cf461477d4a20cabcebd0920da5724371e080a
arthuramarques <jonasnpereira09@gmail.com>
2024-01-04 17:31:36 -0300

    desafio_relacionais

---
+++ Fundamentos/Operadores/desafios_relacionais.js
@@ -0,0 +1,15 @@
+let a = 1;
+let b = 2; 
+
+// encontrar cinco expressões que o resultado é TRUE
+console.log(a <= b);
+console.log(a !== b);
+console.log(b >= a);
+console.log(a != b);
+console.log(a !== b);
+// encontrar cinco expressões que o resultado é FALSE
+console.log(a == b);
+console.log(a === b);
+console.log(b < a);
+console.log(b <= a);
+console.log(a >= b);
