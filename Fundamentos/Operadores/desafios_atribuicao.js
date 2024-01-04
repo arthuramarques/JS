8e948530c4d2c204a77893c10bea978ed9fef265
arthuramarques <jonasnpereira09@gmail.com>
2024-01-04 18:24:38 -0300

    desafio_atribuicao

---
+++ Fundamentos/Operadores/desafios_atribuicao.js
@@ -0,0 +1,8 @@
+// Objetivo é fazer aparecer no console uma lista de aprovados
+let t = "Lista de Aprovados";
+t += "\n==================";
+t += "\n1. Ana Silva";
+t += "\n2. Pedro Albuquerque";
+t += "\n3. Guilherme Pereira";
+t += "\n4. Rebeca França";
+console.log(t);
