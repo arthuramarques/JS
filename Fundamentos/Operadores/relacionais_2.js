a0cbe99a3065f8effe6264d34f04a5874d729c7f
arthuramarques <jonasnpereira09@gmail.com>
2024-01-04 16:43:47 -0300

    operadores relacionais

---
+++ Fundamentos/Operadores/relacionais_2.js
@@ -0,0 +1,11 @@
+// a linguagem JS compara os conteúdos e não os tipos 
+let a = 1;
+let b = "1";
+let c = "1";
+
+console.log(a ==b);
+
+// comparação estrita: interessa saber se os conteúdos são do mesmo tipo e se os conteúdos são iguais
+console.log(a === b); 
+console.log(a != b);
+console.log(a !== b);
