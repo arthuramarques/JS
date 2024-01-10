a4d42c9f033d5d74072212acd1f2d063b53d7440
arthuramarques <jonasnpereira09@gmail.com>
2024-01-10 17:45:50 -0300

    desafio_funcao_1

---
+++ Fundamentos/Funções/desafio_funcao_1.js
@@ -0,0 +1,16 @@
+//funcao: 2 params --> console.log(+)
+//funcao: 2 params --> console.log(-)
+//Chamar as duas funções 
+//Definindo a função somar
+function somar (num1, num2) {
+    console.log(num1 + num2);
+}
+//Definindo a função subtrair
+function subtrair (num1, num2) {
+    console.log(num1 - num2);
+}
+//Chamando a função somar
+somar(133, 144);
+//CHamando a função subtrair
+subtrair(2223, 45);
+//Cada linguagem possui suas regras para a definição de identificadores (nomes-variáveis, funções).
