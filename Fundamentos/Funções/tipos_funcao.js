9d25f439995559130bf82c53ce177c6be681b747
arthuramarques <jonasnpereira09@gmail.com>
2024-01-10 19:14:31 -0300

    tipos_de_funcao_2

--- Fundamentos/Funções/tipos_funcao.js
+++ Fundamentos/Funções/tipos_funcao.js
@@ -1,4 +1,4 @@
-// #1 Função com parâmetro e com retorno
+// #1: Função com parâmetro e com retorno
 function somar(a, b) {
     return a + b;
 }
@@ -6,15 +6,21 @@ let resultado = somar(30, 50);
 console.log(resultado);
 console.log(somar(30, 50));
 
-// #2 Função com parâmetro e sem retorno
+// #2: Função com parâmetro e sem retorno
 function exibirMultiplicação(a, b) {
     console.log(a * b);
 }
 
 exibirMultiplicação (10, 21);
 
-// #3 Função sem parâmetros e com retorno
+// #3: Função sem parâmetros e com retorno
 function retornarDataAtual() {
     return new Date();
 }
 console.log (retornarDataAtual());
+
+// #4: Função sem parâmetros e sem retorno
+function exibirHoraAtual() {
+    console.log(new Date().getHours());
+}
+exibirHoraAtual();
