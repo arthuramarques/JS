f617dc330d9879f834e38219510104f77a6553a8
arthuramarques <jonasnpereira09@gmail.com>
2024-01-10 16:17:34 -0300

    break_continue

---
+++ Fundamentos/Estruturas de controle/break_continue.js
@@ -0,0 +1,16 @@
+//break
+for (let i = 1; i <= 5; i++) {
+    if (i === 3) {
+        break; //Finaliza a repetição.
+    }
+    console.log(i);
+
+}
+
+//continue
+for (let i = 1; i <= 5; i++) {
+    if (i === 3) {
+        continue; //Interrompe uma repetição e inicia uma nova repetição. 
+    }
+    console.log(i);
+}
