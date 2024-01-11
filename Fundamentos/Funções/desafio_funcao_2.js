fc25b49b5d261bdc8727738dd304247953b5be5f
arthuramarques <jonasnpereira09@gmail.com>
2024-01-11 16:34:41 -0300

    desafio_funcao_2

---
+++ Fundamentos/Funções/desafio_funcao_2.js
@@ -0,0 +1,29 @@
+//Parâmetro --> Conceito
+function notaParaConceito(nota) {
+    if (nota === 10) {
+        return 'A+';
+    } else if (nota === 9) {
+        return 'A';
+    } else if (nota === 8) {
+        return 'B+';
+    } else if (nota === 7) {
+        return 'B';
+    } else if (nota === 6) {
+        return 'C+';
+    } else if (nota === 5) {
+        return 'C';
+    } else if (nota === 4) {
+        return 'D+';
+    } else if (nota === 3) {
+        return 'D';
+    } else if (nota === 2) {
+        return 'E+';
+    } else if (nota === 1) {
+        return 'E';
+    } else if (nota === 0) {
+        return 'F';
+    } else {
+        return null 
+    }
+}
+console.log (notaParaConceito(nota = -30));
