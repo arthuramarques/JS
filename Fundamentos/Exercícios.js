d9f43975fc9d8703d40a8c9e4e4ec07d27542d0a
arthuramarques <jonasnpereira09@gmail.com>
2024-01-15 19:33:44 -0300

    exercicios_1

---
+++ Fundamentos/Exercícios.js
@@ -0,0 +1,18 @@
+//01
+function cumprimentar (nome) {
+      return console.log(`Olá, ${nome}`);
+
+};
+
+cumprimentar('Maria');
+cumprimentar('Leonardo');
+//---------------------------------------------//----------------------------------//-----------------------------------//----------
+//02
+console.log('\n');
+
+function converterIdadeEmAnosParaDias (idade) {
+    return console.log(365 * idade);
+}
+converterIdadeEmAnosParaDias(25);
+converterIdadeEmAnosParaDias(70);
+
